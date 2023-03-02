8d2d60fe90fb6c80c972e6e213dfe57ae469a8c6
Kevin Wijesooriya <kevin@hatchyard.io>
2023-03-02 10:22:05 +0530

    implemented the demo page

--- directfn-mubasher/src/App.js
+++ directfn-mubasher/src/App.js
@@ -1,13 +1,24 @@
-import Landing from './views/Main/Landing';
-import PackageSelector from './views/Subscription/PackageSelector';
-import ProductSelector from './views/Subscription/ProductSelector';
+import { Button, Container, Grid, Stack, Typography } from '@mui/material'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import dfnLogo from './assets/images/dfnLogo.png'
 
 function App() {
   return (
-    <ProductSelector />
-    // <PackageSelector />
-    // <Landing />
-  );
+    <>
+      <Container>
+        <Stack spacing={3}>
+          <img alt="dfnLogo" src={dfnLogo} width={250} />
+          <Grid display="flex"
+            flexDirection="column"
+            alignItems="center" ><Typography color={"primary"} fontWeight={700} variant="h4">DEMO</Typography></Grid>
+          <Button component={Link} to="auth/login">Auth</Button>
+          <Button component={Link} to="main/landing">Landing</Button>
+          <Button component={Link} to="subscription/market">Subscription</Button>
+        </Stack>
+      </Container>
+    </>
+  )
 }
 
-export default App;
+export default App
